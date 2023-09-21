e065604ca0e17c3cbc1b75823aca8336959cdb85
FraRussotto <russottofrancesco1@gmail.com>
2023-09-21 14:54:46 +0200

    [edit] Array Team

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//1. Creaiamo l'array con tutti gli oggetti.
+
+const team = [
+  {
+    firstname: 'Wayne',
+    lastname: 'Barnett',
+    role: 'Founder & CEO'
+  },
+  {
+    firstname: 'Angela',
+    lastname: 'Caroll',
+    role: 'Chied Editor'
+  },
+  {
+    firstname: 'Walter',
+    lastname: 'Gordon',
+    role: 'Office Manager'
+  },
+  {
+    firstname: 'Angela',
+    lastname: 'Lopez',
+    role: 'Social Media Manager'
+  },
+  {
+    firstname: 'Scott',
+    lastname: 'Estrada',
+    role: 'Developer'
+  },
+  {
+    firstname: 'Barbara',
+    lastname: 'Ramos',
+    role: 'Graphic Ramos'
+  },
+]
+
+console.log(team)
